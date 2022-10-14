ce0cb8e366591fac6071b6a0a1bcb299cbb891e8
shahidjaber90 <shahidjaber90@yahoo.com>
2022-10-14 21:34:09 +0500

    Task 9 completed

---
+++ app.js
@@ -0,0 +1,66 @@
+//            Task  1 Complate
+
+// Installation complete Nodejs & Vs code
+
+//            Task  2 Complate
+
+// var name1 = "Eric"
+// console.log("Hello " + name1 + ", would you like to learn some Python today?");
+
+//            Task  3 Complate
+
+// let name1 = "Muhammad shahid";
+// console.log(name1.toUpperCase());
+// console.log(name1.toLowerCase());
+
+// function titleCase(string) {
+//     name1 = string.toLowerCase().split(' ');
+//     for (var i = 0; i < name1.length; i++) {
+//       name1[i] = name1[i].charAt(0).toUpperCase() + name1[i].slice(1); 
+//     }
+//     return name1.join(' ');
+//   }
+//   console.log(titleCase(name1));
+
+//            Task  4 Complate
+
+// console.log("Phyllis Diller once said, \"Never go to bed mad. Stay up and fight.\"");
+
+//            Task  5 Complate
+
+// let personeName = "Phyllis Diller";
+// let famousQuote = "\"Never go to bed mad. Stay up and fight.\"";
+// console.log(personeName + " once said, " + famousQuote);
+
+//            Task  6 Complate
+
+// let name1 = "\t Muhammad Shahid \n";
+// console.log(name1);
+
+//            Task  7 Complate
+
+// const num1 = 8;
+// console.log(num1 + 5);
+// console.log(num1 - 3);
+// console.log(num1 * 2);
+// console.log(num1 / 2);
+
+//            Task  8 Complate
+
+// function hrLine(){
+//     let line = document.createElement("hr");
+// document.body.appendChild(line);
+// }
+
+// hrLine();
+// document.write(5+3);
+// hrLine();
+// document.write(5+3);
+// hrLine();
+// document.write(5+3);
+// hrLine();
+
+//            Task  9 Complate
+
+const favNumber = 9;
+console.log(favNumber + " " + "This number is made my life ");
